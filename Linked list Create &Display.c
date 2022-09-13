56d0be882415f8269a3b80c4f3912157c4cc269b
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-09-13 15:49:34 +0600

    Create Linked list Create &Display.c

---
+++ Linked list Create &Display.c
@@ -0,0 +1,69 @@
+#include<stdlib.h>
+#include <stdio.h>
+
+void create();
+void display();
+
+struct node
+{
+        int info;
+        struct node *next;
+};
+struct node *start=NULL;
+int main()
+{
+        int choice,n=5;
+        while(n--){
+
+                create();
+        }
+        display();
+        return 0;
+}
+void create()
+{
+        struct node *temp,*ptr;
+        temp=(struct node *)malloc(sizeof(struct node));
+        if(temp==NULL)
+        {
+                printf("\nOut of Memory Space:\n");
+                exit(0);
+        }
+        printf("\nEnter the data value for the node:\t");
+        scanf("%d",&temp->info);
+        temp->next=NULL;
+        if(start==NULL)
+        {
+                start=temp;
+        }
+        else
+        {
+                ptr=start;
+                while(ptr->next!=NULL)
+                {
+                        ptr=ptr->next;
+                }
+                ptr->next=temp;
+        }
+}
+void display()
+{
+        struct node *ptr;
+        if(start==NULL)
+        {
+                printf("\nList is empty:\n");
+                return;
+        }
+        else
+        {
+                ptr=start;
+                printf("\nThe List elements are:\n");
+                while(ptr!=NULL)
+                {
+                        printf("%d\t",ptr->info );
+                        ptr=ptr->next ;
+                }
+        }
+}
+
+
